e6cd57ebe8c765362dc14de3884abcd7075079dc
Arnd <git@codefanatic.de>
2019-03-04 22:45:04 +0100

    Consider command line options

--- main.py
+++ main.py
@@ -3,9 +3,28 @@ Track the stock performance on the stock market and store price development.
 Trigger notifications to the user in case of high market volatility.
 """
 
-from stock import Stock
 from pprint import pprint
+from stock import Stock
+from getopt import getopt
+import sys
 
+
+def save(stocks):
+    # Download stock performance images
+    for stock in stocks:
+        stock.getImage()
+    # Save stock performance data
+    for stock in stocks:
+        stock.saveData()
+    # Delete stock performance images
+    Stock.deleteImages()
+    # Save stock performance images
+    Stock.saveImages()
+
+def track(stocks):
+    # Retrieve stock performance data
+    for stock in stocks:
+        pprint(stock.getData())
 
 def main():
     # Open connection to stock exchange database
@@ -13,25 +32,20 @@ def main():
     apple = Stock("US0378331005")
     google = Stock("US02079K3059")
     netflix = Stock("US64110L1061")
-    # Retrieve stock performance data
-    pprint(microsoft.getData())
-    pprint(apple.getData())
-    pprint(google.getData())
-    pprint(netflix.getData())
-    # Download stock performance images
-    microsoft.getImage()
-    apple.getImage()
-    google.getImage()
-    netflix.getImage()
-    # Save stock performance data
-    microsoft.saveData()
-    apple.saveData()
-    google.saveData()
-    netflix.saveData()
-    # Delete stock performance images
-    Stock.deleteImages()
-    # Save stock performance images
-    Stock.saveImages()
+    stocks = [microsoft, apple, google, netflix]
+
+    # Execute command line option parsing
+    if len(sys.argv) == 1:
+        print("Error@main: Not enough input arguments given!")
+    else:
+        opts, args = getopt(sys.argv[1:], "sth", ["save", "track", "help"])
+        for opt, arg in opts:
+            if opt in ("-s", "--save"):
+                save(stocks)
+            elif opt in ("-t", "--track"):
+                track(stocks)
+            elif opt in ("-h", "--help"):
+                pass
 
 if __name__ == "__main__":
     main()
